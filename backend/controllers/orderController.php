689c128816e4351cbbbfa3ae390023d1f589079c
LuongHongTienDat <71415613+LuongHongTienDat@users.noreply.github.com>
2022-12-03 08:59:58 +0700

    updqte

--- backend/controllers/orderController.php
+++ backend/controllers/orderController.php
@@ -1,11 +1,23 @@
 <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: Origin,X-Requested-With, Content-Type, Accept");
-    header("Access-Control-Allow-Methods: POST, PUT, DELETE, PATCH, GET");
-    header('Content-Type: application/json; charset=utf-8');
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
 
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
 
     $path = explode('/', parse_url($_SERVER["REQUEST_URI"])["path"]);
 
