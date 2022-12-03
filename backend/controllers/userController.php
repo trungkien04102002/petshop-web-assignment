f0b15b3ccc3069727c1cfd5350b0e67fdc1936e9
LuongHongTienDat <71415613+LuongHongTienDat@users.noreply.github.com>
2022-12-03 07:25:32 +0700

    fix

--- backend/controllers/userController.php
+++ backend/controllers/userController.php
@@ -6,7 +6,7 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Headers: 'Origin,X-Requested-With, Content-Type, Accept'");
     header("Access-Control-Allow-Methods: *");
     header('Content-Type: application/json; charset=utf-8');
 
