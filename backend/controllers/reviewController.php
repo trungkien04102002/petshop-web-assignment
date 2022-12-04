9d079830386d18219165ddb5f55d91b096e26033
Ha Trung Kien <trungkien04102002@gmail.com>
2022-12-04 13:57:28 +0700

    fix patch

--- backend/controllers/reviewController.php
+++ backend/controllers/reviewController.php
@@ -11,7 +11,7 @@
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+            header("Access-Control-Allow-Methods: GET, POST, PATCH, OPTIONS");         
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
